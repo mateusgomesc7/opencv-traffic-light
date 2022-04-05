0847355ac901def66f7873fa9066d4a119d1e7fb
Mateus Gomes <mateusgomesc7@gmail.com>
2022-04-05 09:05:39 -0300

    upddate template match

--- template_match.py
+++ template_match.py
@@ -3,8 +3,43 @@ import cv2 as cv
 import numpy as np
 import glob
 
-
 def find_traffic_light(image):
+    threshold = 0.75
+    method = cv.TM_CCORR_NORMED
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    template = cv.imread('./assets/template-matching/template/model.jpg', 0)
+    template = cv.resize(template, (50, 81))
+    w, h = template.shape[::-1]
+
+    max_values = []
+    min_val, max_val, min_loc, max_loc = 0, 0, 0, 0
+
+    for scale in np.linspace(0.5, 1.5, 20):
+        dim = (int(image_gray.shape[1] * scale), int(image_gray.shape[0] * scale))
+        resized = cv.resize(image_gray, dim)
+
+        res = cv.matchTemplate(resized, template, method)
+        loc = np.where(res >= threshold)
+
+        if len(loc[0]) > 0:
+            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+            max_values.append([max_val, max_loc, scale])
+
+    if len(max_values) > 0:
+        max_val, max_loc, scale = max(max_values, key=lambda item: item[0])
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        image_crop = image[top_left[1]:top_left[1]+h, top_left[0]:top_left[0]+w]
+        if image_crop.shape[0] == 0 or image_crop.shape[1] == 0:
+            return False, False, False
+
+        return image_crop, top_left, bottom_right
+
+    return False, False, False
+
+
+def find_traffic_light_for_test(image):
     threshold = 0.75
     method = cv.TM_CCORR_NORMED
     image_color = image.copy()
@@ -33,7 +68,7 @@ def find_traffic_light(image):
         bottom_right = (top_left[0] + w, top_left[1] + h)
 
         image_crop = image_color[top_left[1]:top_left[1]+h, top_left[0]:top_left[0]+w]
-        if image_crop.shape[0] == 0:
+        if image_crop.shape[0] == 0 or image_crop.shape[1] == 0:
             return False, False, False, False
         cv.rectangle(image_color, top_left, bottom_right, (255, 255, 255), 2)
 
@@ -51,7 +86,7 @@ if __name__ == "__main__":
 
     # inicio = time.time()
 
-    # image_crop, top_left, bottom_right, image = find_traffic_light(image)
+    # image_crop, top_left, bottom_right, image = find_traffic_light_for_test(image)
 
     # final = time.time()
     # print(f'Encontrar semáforo: {(final - inicio):.2f} segundos')
@@ -71,10 +106,11 @@ if __name__ == "__main__":
     for img in images:
         image = cv.imread(img)
         image = cv.resize(image, (400, 300))
-        image_crop, top_left, bottom_right, image_gray = find_traffic_light(image)
+        image_crop, top_left, bottom_right, image_gray = find_traffic_light_for_test(image)
         if image_crop is False:
            count_match += 1
-        cv.imshow(f'{img}', image_gray)
+        else:
+            cv.imshow(f'{img}', image_gray)
 
     print(f'Quantidade de semáforos encontrados: {len(images)-count_match}/{len(images)}')
     cv.waitKey(0)
