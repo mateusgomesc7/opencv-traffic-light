67b3068ff38967c1fc83390e9b15345adb30651d
Mateus Gomes <mateusgomesc7@gmail.com>
2022-04-21 17:27:13 -0300

    change haarcascade

--- haarcascade.py
+++ haarcascade.py
@@ -6,8 +6,8 @@ def find_traffic_light(image):
     image_crop = None
     cascade = cv.CascadeClassifier('./cascade.xml')
     # image = cv.resize(image, (400, 300))
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    faces = cascade.detectMultiScale(image_gray, 1.3, 5)
+    # image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces = cascade.detectMultiScale(image, 1.3, 5)
     # width, height = template.shape[::-1] 
     for (x,y,w,h) in faces:
         top_left = (x, y)
@@ -19,17 +19,23 @@ largura_img = 160
 altura_img = 120
 
 if __name__ == "__main__":
-    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    cap = cv.VideoCapture(0)
+    
+    cv.namedWindow("image", cv.WINDOW_NORMAL)
+    cv.resizeWindow("image", 400, 300)
     
     while(True):
         _, frame = cap.read()
         frame = cv.resize(frame, (largura_img, altura_img))
         
-        image_crop, top_left, bottom_right = find_traffic_light(frame)
-        print('shape', frame.shape, top_left, bottom_right)
+        # Tratamento da Imagem
+        image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        image = cv.blur(image, (1, 1))
+        
+        image_crop, top_left, bottom_right = find_traffic_light(image)
         if top_left:
             cv.rectangle(frame,top_left,bottom_right,(255,255,255),1)
-        cv.imshow('frame',frame)
+        cv.imshow('image',frame)
         
         key = cv.waitKey(1)
         
